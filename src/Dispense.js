7ef7bca0a756961b4a7a2acd79e74ef08e1bce39
Mats Fjeld <matsfj@student.matnat.uio.no>
2021-11-01 15:10:44 +0100

    show "End Balance" of commodities in table view

--- src/Dispense.js
+++ src/Dispense.js
@@ -1,5 +1,16 @@
 import React from "react";
+import classes from "./App.module.css";
 import { FlyoutMenu, MenuItem } from "@dhis2/ui";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableCellHead,
+  TableFoot,
+  TableHead,
+  TableRow,
+  TableRowHead,
+} from '@dhis2/ui'
 
 
 const organisationUnit = "AlLmKZIIIT4";
@@ -9,16 +20,44 @@ const lifeSavingCommodeties = "ULowA8V3ucd";
 import { useDataQuery } from '@dhis2/app-runtime'
 
 const dataQuery = {
-  dataSets: {
+
+  dataValueSets: {
     resource: 'dataSets/' + lifeSavingCommodeties,
     params: {
       fields: [
         'name',
         'id',
         'dataSetElements[dataElement[id, displayName]',
+
       ],
     },
   },
+
+  dataSets: {
+    resource: 'dataValueSets',
+    "params": {
+      "orgUnit": organisationUnit,
+      "dataSet": lifeSavingCommodeties,
+      "period": "202110",
+    }
+  }
+}
+
+function mergeData(data) {
+
+  return data.dataSets.dataValues.map(d => {
+    let match = data.dataValueSets.dataSetElements.find(dataSetElement => {
+      if (dataSetElement.dataElement.id == d.dataElement) {
+        return true
+      }
+    })
+    return {
+      id: match.dataElement.id,
+      displayName: match.dataElement.displayName.replace("Commodities - ", ""), // only show name of commodity
+      value: d.value,
+      categoryOptionCombo: d.categoryOptionCombo
+    }
+  })
 }
 
 
@@ -34,20 +73,38 @@ export function Dispense() {
   }
 
   if (data) {
-    let names = data.dataSets["dataSetElements"];
+
+    console.log(data)
+
+    // rQLFnNXXIL0 = End Balance
+    const stock = mergeData(data).filter(e => e.categoryOptionCombo == "rQLFnNXXIL0").sort((a, b) => {
+      return b.value - a.value;
+    });
+    console.log(stock)
 
     return (
       <div>
         <h1>Life saving commodeties at {organisationUnit}</h1>
-        <FlyoutMenu>
-          {
-            names.map(row => {
+        <h2>Stock </h2>
+        <Table>
+          <TableHead>
+            <TableRowHead>
+              <TableCellHead>Commodity</TableCellHead>
+              <TableCellHead>Stock</TableCellHead>
+            </TableRowHead>
+          </TableHead>
+          <TableBody>
+            {stock.map(row => {
               return (
-                <MenuItem key={row["dataElement"].id} label={row["dataElement"].displayName} />
+                <TableRow key={row.id}>
+                  <TableCell>{row.displayName}</TableCell>
+                  <TableCell>{row.value}</TableCell>
+                </TableRow>
               )
             })
-          }
-        </FlyoutMenu>
+            }
+          </TableBody>
+        </Table>
       </div>
     );
   }
